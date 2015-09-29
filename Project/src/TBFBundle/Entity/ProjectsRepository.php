0c4a3c2240a15296235edb4b8c12fce07af68541
vincent <madiori44@gmail.com>
2015-09-29 12:10:25 +0200

    search function is corrected

--- Project/src/TBFBundle/Entity/ProjectsRepository.php
+++ Project/src/TBFBundle/Entity/ProjectsRepository.php
@@ -36,16 +36,16 @@ class ProjectsRepository extends EntityRepository
         public function getProjectByName($name){
         try {
             $qb = $this->createQueryBuilder('p');
-            return $qb      //->leftJoin('p.members', 'usrid')
-                            //->addSelect('usrid')
-                            //->leftJoin('p.skills', 'skillid')
-                            //->addSelect('skillid')
+            return $qb      ->leftJoin('p.members', 'usrid')
+                            ->addSelect('usrid')
+                            ->leftJoin('p.skills', 'skillid')
+                            ->addSelect('skillid')
                             ->where($qb->expr()->like('p.name', ':name'))
                             ->setParameter('name', '%' . $name . '%')
                             ->getQuery()
                             ->getResult();
         } catch (\Doctrine\DBAL\DBALException $e) {
-            //echo $e->getTraceAsString();
+            echo $e->getTraceAsString();
         }
     }
 }
